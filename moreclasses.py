4c848c8af79b0a2ecf51573804ed1e0de096fdd5
H2212 <viktorholyak1@gmail.com>
2026-03-15 10:47:39 +0200

    jsi

---
+++ moreclasses.py
@@ -0,0 +1,8 @@
+class Human:
+    def __init__(self, name = "Human"):
+        self.name = name
+
+class Auto:
+    def __init__(self, brand):
+        self.brand = brand
+        self.passengers = []
