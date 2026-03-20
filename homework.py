d830fab541d42f814930d5c638e46e11ea55a6ca
SteppingAndrii <im.not.andriii@gmail.com>
2026-03-20 11:29:10 +0200

    Made it cleaner

--- homework.py
+++ homework.py
@@ -1,46 +1,33 @@
-name = "Larry"
-
 class Student:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.grade = 88
-
     def study(self):
-        print(f"{name}'s grades are {self.grade}%")
-
+        print(f"{self.name}'s grades are {self.grade}%")
 
 class Worker:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.job = "Programmer"
         self.salary = 4500
-
     def work(self):
-        print(f"{name} works as a {self.job} and earns {self.salary}$ per month")
-
+        print(f"{self.name} works as a {self.job} and earns {self.salary}$ per month")
 
 class Athlete:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.sport = "Football"
         self.energy = 89
-  
     def train(self):
-        print(f"{name}'s sport is {self.sport} and {name}'s energy is {self.energy}%")
+        print(f"{self.name}'s sport is {self.sport} and {self.name}'s energy is {self.energy}%")
 
-
-# All in one person
 class Person(Student, Worker, Athlete):
-    def __init__(self):
-        Student.__init__(self)
-        Worker.__init__(self)
-        Athlete.__init__(self)
-
+    def __init__(self, name):
+        Student.__init__(self, name)
+        Worker.__init__(self, name)
+        Athlete.__init__(self, name)
     def show_all_info(self):
-        print(f"Your info:\nName: {name}\nGrade: {self.grade}\nJob: {self.job}\nSport: {self.sport}\nEnergy: {self.energy}%")
+        print(f"Your info:\nName: {self.name}\nGrade: {self.grade}\nJob: {self.job}\nSport: {self.sport}\nEnergy: {self.energy}%")
 
-p = Person()
-
-# p.study()     # from Student
-# p.work()      # from Worker
-# p.train()     # from Athlete
-
-# All in one info
+p = Person("Larry")
 p.show_all_info()
